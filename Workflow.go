52824484328b7f7732a24b13fbb7360341a7dc1e
singhjas <singhjas@msoe.edu>
2026-03-03 16:11:32 -0600

    Intial setup

---
+++ Workflow.go
@@ -0,0 +1,21 @@
+Workflow will be:
+git checkout -b "Name of branch"
+git add .
+git commit -m "Message"
+
+git checkout main
+git merge feature/database-setup
+git push origin main
+git branch -d "Whatever the name will be like: feature/database-setup"
+
+IF I accidently deleted my code, I can recover run:
+git checkout main -- .
+
+To undue changes to a file, I can run:
+git restore <file_name>
+
+To undo all changes in the folder, I can run:
+git restore .
+
+IF I want to pull specifically from my main branch, I can run:
+git restore --source=main <file_name>
